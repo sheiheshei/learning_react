d943758bc3cedcf3e3430843422d38391b41c36e
wangxu <1025822387@qq.com>
2021-01-30 16:05:10 +0800

    添加相关标签和函数的解释

--- react_hello/router.js
+++ react_hello/router.js
@@ -4,99 +4,14 @@ let Link = ReactRouterDOM.Link
 let Router = ReactRouterDOM.BrowserRouter
 let useParams = ReactRouterDOM.useParams
 
-
-// router的基本使用，使用Link标签将元素渲染成<a><a/>标签
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <nav>
-//           <ul>
-//             <li>
-//               <Link to="/">Home</Link>
-//             </li>
-//             <li>
-//               <Link to="/about">About</Link>
-//             </li>
-//             <li>
-//               <Link to="/users">Users</Link>
-//             </li>
-//           </ul>
-//         </nav>
-
-//         {/* A <Switch> looks through its children <Route>s and
-//             renders the first one that matches the current URL. */}
-//         <Switch>
-//           <Route path="/about">
-//             <About />
-//           </Route>
-//           <Route path="/users">
-//             <Users />
-//           </Route>
-//           <Route path="/">
-//             <Home />
-//           </Route>
-//         </Switch>
-//       </div>
-//     </Router>
-//   );
-// }
-
-
-// function App() {
-//   return (
-//     <div>
-//       <Switch>
-//         {/* If the current URL is /about, this route is rendered
-//             while the rest are ignored */}
-//         <Route path="/about">
-//           <About />
-//         </Route>
-
-//         {/* Note how these two routes are ordered. The more specific
-//             path="/contact/:id" comes before path="/contact" so that
-//             route will render when viewing an individual contact */}
-//         <Route path="/contact/:id">
-//           <Contact />
-//         </Route>
-//         <Route path="/contact">
-//           <AllContacts />
-//         </Route>
-
-//         {/* If none of the previous routes render anything,
-//             this route acts as a fallback.
-
-//             Important: A route with path="/" will *always* match
-//             the URL because all URLs begin with a /. So that's
-//             why we put this one last of all */}
-//         <Route path="/">
-//           <Home />
-//         </Route>
-//       </Switch>
-//     </div>
-//   );
-// }
-
-// ReactDOM.render(
-//   <Router>
-//     <App />
-//   </Router>,
-//   document.getElementById("complex")
-// );
-
-// function Home() {
-//   return <h2>Home</h2>;
-// }
-
-// function About() {
-//   return <h2>About</h2>;
-// }
-
-// function Users() {
-//   return <h2>Users</h2>;
-// }
-
-// ReactDOM.render(<App/>, document.getElementById("complex"))
+/**
+ * <Link><Link/>会将元素渲染成<a/>标签，to属性表示其去往那个URL，
+ * <Router></Router>,内部是一组路由，组成路由器
+ * <Switch></Switch>，这个标签表示其中包含的多个route标签只会渲染其中一个route标签包含的内容，如果匹配多个route标签，只有第一个会被渲染
+ * <Route><Route/> 包含一个或者多个组件，url中的值匹配其中的path属性，其中包含的内容就会渲染
+ * useParams,是个函数，在子组件中调用这个函数，能够获取url中参数，父组件中的<Route></Route>标签中的path属性通过：定义这个变量
+ * useRouteMatch,是个函数，子组件中使用URL或者path属性，可以调用这个函数直接获取
+ */
 
 
 // ================================== Test for React Router  =============================================
